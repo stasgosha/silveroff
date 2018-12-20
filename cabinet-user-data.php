2da8ebe891f2a860003f2a819d4408ecc5beaac9
Eugene Merkulove <merkuloveko@gmail.com>
2018-12-20 17:02:39 +0300

    сchange void

--- cabinet-user-data.php
+++ cabinet-user-data.php
@@ -26,21 +26,14 @@
 					<div class="user-cabinet-component">
 						<div class="component-sidebar">
 							<ul class="cabinet-nav">
-								<li><i class="icon-document" aria-hidden="true"></i><a href="#">Мои заказы</a></li>
-								<li class="current-menu-item"><i class="icon-user" aria-hidden="true"></i><a href="#">Мои данные</a></li>
-								<li><i class="icon-envelope" aria-hidden="true"></i><a href="#">Обратная связь</a></li>
-								<li><i class="icon-logout" aria-hidden="true"></i><a href="#">Выйти из ЛК</a></li>
+								<li class="right-triangle"><a href="#"><i class="icon-document" aria-hidden="true"></i>Мои заказы</a></li>
+								<li class="current-menu-item"><a href="#"><i class="icon-user" aria-hidden="true"></i>Мои данные</a></li>
+								<li><a href="#"><i class="icon-envelope" aria-hidden="true"></i>Обратная связь</a></li>
+								<li class="left-triangle"><a href="#"><i class="icon-logout" aria-hidden="true"></i>Выйти из ЛК</a></li>
 							</ul>	
 							<div class="loyalty-card">
 								<div class="card-img">
-									<h3>SILVEROFF</h3>
-									<p class="letter-space">серебро с мужским характером</p>
-									<p>
-										<span>0015</span>
-										<span>1034</span>
-										<span>4432</span>
-										<span>5412</span>
-									</p>
+									<img src="img/content-page/logo_card.jpg" alt="">
 								</div>
 								<div class="card-score">
 									<h4>Накопленные баллы:</h4>
@@ -50,60 +43,62 @@
 										<span>5</span>
 										<span>9</span>
 									</p>
-									<p>Как потратить?<i class="icon-info"></i></p>
+									<a>Как потратить?<i class="icon-info"></i></a>
 								</div>
 							</div>
 						</div>
 						<div class="component-content">
-							<h4>Ваши данные</h4>
-							<div class="form-wrapp-row">
-								<div class="form-field">
-									<input id="user-name" type="text" required class="input-field">
-									<label for="user-name">Имя пользователя</label>
-									<p class="slf-note">Имя пользователя, например ivan_petrov</p>
+							<h3 class="slf-h3 font-300 with-corner">Ваши данные</h3>
+							<form action="#" class="user-data-form form">
+								<div class="form-wrapp-row">
+									<div class="form-field">
+										<input id="user-name" type="text" required class="input-field">
+										<label for="user-name">Имя пользователя</label>
+										<p class="slf-note min-line-height">Имя пользователя, например ivan_petrov</p>
+									</div>
+									<div class="form-field">
+										<input id="user-name" type="text" required class="input-field">
+										<label for="user-name">ФИО*</label>
+										
+									</div>
 								</div>
-								<div class="form-field">
-									<input id="user-name" type="text" required class="input-field">
-									<label for="user-name">ФИО</label>
-									
+								<div class="form-wrapp-row">
+									<div class="form-field">
+										<input id="user-email" type="email" required class="input-field">
+										<label for="user-email">Ваш e-mail*</label>
+									</div>
+									<div class="form-field">
+										<input id="user-phone" type="phone" required class="input-field">
+										<label for="user-phone">Телефон*</label>
+									</div>
 								</div>
-							</div>
-							<div class="form-wrapp-row">
-								<div class="form-field">
-									<input id="user-email" type="email" required class="input-field">
-									<label for="user-email">Ваш e-mail*</label>
+								<div class="form-wrapp-column">
+									<div class="form-field">
+										<input id="password" type="password" required class="input-field">
+										<label for="password">Пароль</label>
+										<p class="slf-note min-line-height">Вы можете указать новый пароль</p>
+									</div>
+									<div class="form-field">
+										<input id="password_confirm" type="password" required class="input-field">
+										<label for="password">Повторите пароль</label>
+										<p class="slf-note min-line-height">
+											Нужно повторить новый пароль еще раз, чтобы исключить опечатку
+										</p>
+									</div>
 								</div>
-								<div class="form-field">
-									<input id="user-phone" type="phone" required class="input-field">
-									<label for="user-phone">Телефон*</label>
-								</div>
-							</div>
-							<div class="form-wrapp-column">
-								<div class="form-field">
-									<input id="password" type="password" required class="input-field">
-									<label for="password">Пароль</label>
-									<p class="slf-note">Вы можете указать новый пароль</p>
-								</div>
-								<div class="form-field">
-									<input id="password" type="password" required class="input-field">
-									<label for="password">Повторите пароль</label>
-									<p class="slf-note">
-										Нужно повторить новый пароль еще раз, чтобы исключить опечатку
-									</p>
-								</div>
-							</div>
+							<form>
 							<div class="user-avatar">
-								<h4>Аватар</h4>
-								<div class="component-avatar"> 
+								<h3 class="slf-h3 font-300 with-corner">Аватар</h3>
+								<div class="select-avatar-block"> 
 									<div class="user-photo">
-										<img src="" alt="USER-AVATAR">
+										<img src="img/content-page/avatar.jpg" alt="USER-AVATAR">
 									</div>
-									<div >
+									<div class="block-content">
 										<p clas="slf-note">Если вы не загрузине собственную картинку, аватар будет получен автоматиески из сервиса <a href="#">Gravatar</a></p>
-										<a class="btn btn-bordered btn-small" href="#">Обзор картинки</a>
+										<button class="btn btn-bordered btn-small" href="#">Обзор картинки</button>
 									</div>
 								</div>
-								<a class="btn btn-gray" href="#">Сохранить изменения</a>
+								<button type="submit" class="btn btn-gray" href="#">Сохранить изменения</button>
 							</div>
 						</div>
 					</div>
